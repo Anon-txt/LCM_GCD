7fd568113d72232fcd0978a6a01932acb7ed0d4e
Anon <24657656+Anon20198@users.noreply.github.com>
2018-03-08 21:56:21 +0100

    Update common.java

--- src/common.java
+++ src/common.java
@@ -1,83 +1,70 @@
 public class common {
   public long LCM(int[] x) {
     return getLCM(x);
-}
+  }
 
-private long getLCM (int[] x) {
-
-    long lcm_of_array_elements = 1;
-    int divisor = 2;
-
-        while (true) {
-
+  private long getLCM (int[] x) {
+      long lcm_of_array_elements = 1;
+      int divisor = 2;
+          while (true) {
             int counter = 0;
             boolean divisible = false;
-
-                for (int i = 0; i < x.length; i++) {
-
-                    if (x[i] == 0) {
-                        return 0;
-                    } else if (x[i] < 0) {
-                        x[i] = x[i] * (-1);
-                    }
-
-                        if (x[i] == 1) {
-                            counter++;
-                        }
-
-                            if (x[i] % divisor == 0) {
-                                divisible = true;
-                                x[i] = x[i] / divisor;
-                            }
+              for (int i = 0; i < x.length; i++) {
+                if (x[i] == 0) {
+                  return 0;
+                }else if (x[i] < 0) {
+                  x[i] = x[i] * (-1);
                 }
-
-            if (divisible) {
-                lcm_of_array_elements = lcm_of_array_elements * divisor;
-            } else {
-                divisor++;
-            }
-
-                if (counter == x.length) {
+                if (x[i] == 1) {
+                  counter++;
+                }
+                if (x[i] % divisor == 0) {
+                  divisible = true;
+                  x[i] = x[i] / divisor;
+                }
+              }
+                if (divisible) {
+                  lcm_of_array_elements = lcm_of_array_elements * divisor;
+                }else {
+                  divisor++;
+                }
+                  if (counter == x.length) {
                     return lcm_of_array_elements;
-                }
-        }
-}
+                  }
+          }
+  }
 
-public int GCD(int[] x) {
-    return getGCD(x);
-}
+  public int GCD(int[] x) {
+      return getGCD(x);
+  }
 
-public double GCD(double[] x) {
-    return getGCD(x);
-}
+  public double GCD(double[] x) {
+      return getGCD(x);
+  }
 
-private double getGCD (double[] x) {
+  private double getGCD (double[] x) {
     double result = x[0];
-
-    for (int i = 1; i < x.length; i++)
+      for (int i = 1; i < x.length; i++)
         result = gcd(x[i], result);
+      return result;
+  }
 
-    return result;
-}
-
-private double gcd (double a, double b) {
+  private double gcd (double a, double b) {
     if (a == 0)
-        return b;
+      return b;
     return gcd(b%a, a);
-}
+  }
 
-private int getGCD (int[] x) {
+  private int getGCD (int[] x) {
     int result = x[0];
+      for (int i = 1; i < x.length; i++)
+        result = gcd(x[i], result);
+      return result;
+  }
 
-        for (int i = 1; i < x.length; i++)
-            result = gcd(x[i], result);
-
-    return result;
-}
-
-private int gcd (int a, int b) {
+  private int gcd (int a, int b) {
     if (a == 0)
-        return b;
+      return b;
     return gcd(b%a, a);
-}
+  }
 }
